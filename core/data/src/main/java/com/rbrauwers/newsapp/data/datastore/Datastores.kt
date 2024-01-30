22c4296ad4d08070ea1b83cdbbc024c9cbc42580
rbrauwers <rodrigo.brauwers@dock.tech>
2024-01-30 09:20:23 -0300

    Simple change

--- core/data/src/main/java/com/rbrauwers/newsapp/data/datastore/Datastores.kt
+++ core/data/src/main/java/com/rbrauwers/newsapp/data/datastore/Datastores.kt
@@ -5,7 +5,7 @@ import androidx.datastore.dataStore
 import com.rbrauwers.newsapp.common.CryptoManager
 import com.rbrauwers.newsapp.data.serializers.UserSettingsSerializer
 
-val Context.userSettingsDataStore by dataStore(
+internal val Context.userSettingsDataStore by dataStore(
     fileName = "user-settings.json",
     serializer = UserSettingsSerializer(CryptoManager())
 )
