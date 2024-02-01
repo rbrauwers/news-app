5ff2ca0be11d506c39f0f55f353e3cd995dc9c7c
rbrauwers <rodrigo.brauwers@dock.tech>
2024-02-01 09:33:23 -0300

    TDD - First test definition

---
+++ app/src/test/java/com/rbrauwers/newsapp/ResetPasswordTests.kt
@@ -0,0 +1,13 @@
+package com.rbrauwers.newsapp
+
+import org.junit.Assert
+import org.junit.Test
+
+class ResetPasswordTests {
+
+    @Test
+    fun correctEmailShouldPass() {
+        Assert.assertTrue(false)
+    }
+
+}
