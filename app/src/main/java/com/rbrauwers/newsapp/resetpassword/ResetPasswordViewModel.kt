6f1eaf25991288f7efb3f1088f9a0d6387c83064
rbrauwers <rodrigo.brauwers@dock.tech>
2024-02-02 09:02:53 -0300

    TDD - typedEmailShouldBeCollected REFACTOR 2

--- app/src/main/java/com/rbrauwers/newsapp/resetpassword/ResetPasswordViewModel.kt
+++ app/src/main/java/com/rbrauwers/newsapp/resetpassword/ResetPasswordViewModel.kt
@@ -1,9 +1,12 @@
 package com.rbrauwers.newsapp.resetpassword
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
@@ -11,7 +14,11 @@ import javax.inject.Inject
 class ResetPasswordViewModel @Inject constructor(): ViewModel() {
 
     private val _uiState = MutableStateFlow(UiState())
-    val uiState = _uiState.asStateFlow()
+    val uiState = _uiState.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5000),
+        initialValue = UiState()
+    )
 
     fun update(email: String) {
         _uiState.update {
--- app/src/test/java/com/rbrauwers/newsapp/ResetPasswordViewModelTest.kt
+++ app/src/test/java/com/rbrauwers/newsapp/ResetPasswordViewModelTest.kt
@@ -1,6 +1,17 @@
 package com.rbrauwers.newsapp
 
+import app.cash.turbine.test
 import com.rbrauwers.newsapp.resetpassword.ResetPasswordViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
@@ -9,16 +20,34 @@ internal class ResetPasswordViewModelTest {
 
     private lateinit var viewModel: ResetPasswordViewModel
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Before
     fun setUp() {
+        Dispatchers.setMain(StandardTestDispatcher())
         viewModel = ResetPasswordViewModel()
     }
 
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun typedEmailShouldBeCollected() {
-        val email = "some@email.com"
-        viewModel.update(email)
-        Assert.assertEquals(email, viewModel.uiState.value.email)
+    fun typedEmailShouldBeCollected() = runTest {
+        val job = backgroundScope.launch(UnconfinedTestDispatcher(testScheduler)) {
+            viewModel.uiState.collect()
+        }
+
+        viewModel.uiState.test {
+            // Skip initial value
+            skipItems(1)
+            val email = "some@email.com"
+            viewModel.update(email)
+            Assert.assertEquals(email, awaitItem().email)
+            job.cancel()
+        }
     }
 
 }
