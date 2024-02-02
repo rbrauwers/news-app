0b542906b025f27bd75da7539b5e36df108d4ce4
rbrauwers <rodrigo.brauwers@dock.tech>
2024-02-02 08:47:13 -0300

    TDD - typedEmailShouldBeCollected REFACTOR

--- app/src/main/java/com/rbrauwers/newsapp/resetpassword/ResetPasswordViewModel.kt
+++ app/src/main/java/com/rbrauwers/newsapp/resetpassword/ResetPasswordViewModel.kt
@@ -1,14 +1,20 @@
 package com.rbrauwers.newsapp.resetpassword
 
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import javax.inject.Inject
 
-class ResetPasswordViewModel {
+@HiltViewModel
+class ResetPasswordViewModel @Inject constructor(): ViewModel() {
 
-    val uiState = MutableStateFlow(UiState())
+    private val _uiState = MutableStateFlow(UiState())
+    val uiState = _uiState.asStateFlow()
 
     fun update(email: String) {
-        uiState.update {
+        _uiState.update {
             it.copy(email = email)
         }
     }
--- app/src/test/java/com/rbrauwers/newsapp/ResetPasswordViewModelTest.kt
+++ app/src/test/java/com/rbrauwers/newsapp/ResetPasswordViewModelTest.kt
@@ -5,7 +5,7 @@ import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 
-class ResetPasswordViewModelTest {
+internal class ResetPasswordViewModelTest {
 
     private lateinit var viewModel: ResetPasswordViewModel
 
