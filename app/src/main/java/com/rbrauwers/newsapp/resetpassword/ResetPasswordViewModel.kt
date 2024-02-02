1e1131ae4c90dd361f4037d7d5aba2c66b012a0a
rbrauwers <rodrigo.brauwers@dock.tech>
2024-02-02 08:44:08 -0300

    TDD - typedEmailShouldBeCollected GREEN

--- app/src/main/java/com/rbrauwers/newsapp/resetpassword/ResetPasswordViewModel.kt
+++ app/src/main/java/com/rbrauwers/newsapp/resetpassword/ResetPasswordViewModel.kt
@@ -1,13 +1,16 @@
 package com.rbrauwers.newsapp.resetpassword
 
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
 
 class ResetPasswordViewModel {
 
     val uiState = MutableStateFlow(UiState())
 
     fun update(email: String) {
-
+        uiState.update {
+            it.copy(email = email)
+        }
     }
 
     data class UiState(val email: String = "")
